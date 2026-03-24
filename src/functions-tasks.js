d2b6775dce7a359098300e0dff76b015ed83c216
Olga Rekun <rekun.olya@gmail.com>
2026-03-24 16:25:56 +0100

    feat: add a solution to task partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -171,8 +171,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function name(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
